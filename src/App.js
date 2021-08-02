1a91f6e0f2061163887e808e1ad29934fa12ac77
Marcelo Laos <mlaos1997@gmail.com>
2021-08-01 23:02:29 -0400

    completed mvp, will continue styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+import Character from './components/Character';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,7 +15,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">React Wars</h1>
+      <Character />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,63 @@
 // Write your Character component here
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components';
+
+const Styles = styled.div`
+
+color: white;
+
+ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+li {
+    list-style: none;
+    width: 65%;
+    border: 1px solid green;
+    display: flex;
+    justify-content: space-between;
+    margin: 8px 0;
+    padding-left: 4px;
+}
+
+h2 {
+    font-size: 30px;
+
+}
+
+button {
+    width: 10%;
+    padding: 3%;
+}
+
+`
+const Character = () => {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://swapi.dev/api/people/')
+            .then(res => {
+                console.log(res.data);
+                setCharacters(res.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <Styles className='character-container'>
+            <ul>
+                {characters.map(character => (
+                    <li key={character.created}>
+                        <h2>{character.name}</h2>
+                        <button>{character.birth_year}</button>
+                    </li>
+                ))}
+            </ul>
+        </Styles>
+    )
+};
+
+export default Character;
